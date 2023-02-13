2a4e283b240d75d92c356c239674059026887ef1
chunleili <li_cl@foxmail.com>
2023-02-13 10:54:00 +0800

    armadillo

--- ref/meshtaichi/lag_mpm/run.py
+++ ref/meshtaichi/lag_mpm/run.py
@@ -34,13 +34,11 @@ def init(x, y, i):
     model[0] = transform(model[0], model_size, [x, y, 0.05 + (model_size / 2 + 0.012) * i])
     models.append(model)
 
-n_armadillo = 1
-
 init(0.5, 0.5, 0)
 
 
-mesh = Patcher.load_mesh(models, relations=["CV"])
-fems.append(FEM(mesh))
+armadillo = Patcher.load_mesh(models, relations=["CV"])
+fems.append(FEM(armadillo))
 
 
 ground_model = "models/ground.obj"
@@ -58,6 +56,17 @@ def init_indices_surf(mesh: ti.template(), indices: ti.template()):
 
 init_indices_surf(ground, ground_indices)
 
+armadillo_indices = ti.field(ti.u32, shape = len(armadillo.cells) * 4 * 3)
+@ti.kernel
+def init_indices_tet(mesh: ti.template(), indices: ti.template()):
+    for c in mesh.cells:
+        ind = [[0, 2, 1], [0, 3, 2], [0, 1, 3], [1, 2, 3]]
+        for i in ti.static(range(4)):
+            for j in ti.static(range(3)):
+                indices[(c.id * 4 + i) * 3 + j] = c.verts[ind[i][j]].id
+
+init_indices_tet(armadillo, armadillo_indices)
+
 
 window = ti.ui.Window("virtual surgery", (1920, 1080))
 canvas = window.get_canvas()
@@ -71,6 +80,7 @@ camera.fov(75)
 
 frame = 0
 paused = ti.field(int, shape=())
+paused[None] = 1
 while window.running:
 
     for e in window.get_events(ti.ui.PRESS):
@@ -86,8 +96,9 @@ while window.running:
     camera.track_user_inputs(window, movement_speed=0.0005, hold_key=ti.ui.RMB)
     scene.set_camera(camera)
     
-    scene.particles(fems[0].x, 1e-3, color = (0.5, 0.5, 0.5))
+    scene.particles(fems[0].x, 1e-4, color = (0.5, 0.5, 0.5))
     scene.mesh(ground.verts.x, ground_indices, color = (0.5,0.5,0.5))
+    scene.mesh(fems[0].x, armadillo_indices, color = (232/255, 190/255, 172/255))
 
     scene.point_light(pos=(0.5, 1.5, 0.5), color=(1, 1, 1))
     scene.ambient_light((0.2,0.2,0.2))
